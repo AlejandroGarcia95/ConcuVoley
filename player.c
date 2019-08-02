288e3ce5b39b640f620222a8ec6d5c192bd5c9f0
AlejandroGarcia95 <alemgarcia95@gmail.com>
2019-08-02 09:25:38 -0300

    Changed checkpoint format for newest implementation

--- player.c
+++ player.c
@@ -18,7 +18,7 @@
 #include "semaphore.h"
 #include "tournament.h"
 
-// @loginclude
+// @has_checkpoints
 
 // In microseconds!
 #define MIN_SCORE_TIME 900
@@ -204,7 +204,7 @@ void player_unset_sigset_handler() {
 void player_play_set(unsigned long int* set_score){
 	player_t* player = player_get_instance();
 	if(!player) return;
-	// @log play_set player->id
+	// @checkpoint play_set player->id
 	while(player_is_playing()){
 		emulate_score_time();
 		(*set_score)++;
@@ -215,7 +215,7 @@ void player_play_set(unsigned long int* set_score){
 /* Call this function once player has found a court to join.
  * Makes the player play every set and leave when necessary.*/
 void player_at_court(player_t* player, int court_fifo, int player_fifo) {
-	// @log waiting_at_court player->id
+	// @checkpoint waiting_at_court player->id
 	message_t msg = {};
 	char* p_name = player->name;
 	while(msg.m_type != MSG_MATCH_END){
@@ -248,7 +248,7 @@ void player_at_court(player_t* player, int court_fifo, int player_fifo) {
 			// but other players that arrived couldn't make a team with them.
 			// Hence, court kicked every player there
 			log_write(INFO_L, "Player %03d: Kicked from previously accepted court\n", player->id);
-			// @log couldnt_make_team_at_court player->id 
+			// @checkpoint couldnt_make_team_at_court player->id 
 			player->times_kicked++;
 			return;
 		} else {
@@ -256,13 +256,13 @@ void player_at_court(player_t* player, int court_fifo, int player_fifo) {
 			miss_count++;
 			if (miss_count >= 2) {
 				log_write(INFO_L, "Player %03d: Kicked from previously accepted court\n", player->id);
-				// @log couldnt_make_team_at_court player->id
+				// @checkpoint couldnt_make_team_at_court player->id
 				player->times_kicked++;
 			}
 		}
 	}
 	
-	// @log finish_match player->id
+	// @checkpoint finish_match player->id
 	if(msg.m_type == MSG_MATCH_END) // Not needed for now, but good sanity check
 		player->matches_played++;
 }
@@ -274,7 +274,7 @@ void player_at_court(player_t* player, int court_fifo, int player_fifo) {
  * court found), or with the player joining the court, calling the
  * function player_at_court.*/
 void player_join_court(player_t* player, unsigned int court_id) {
-	// @log found_court player->id  court_id
+	// @checkpoint found_court player->id  court_id
 	log_write(INFO_L, "Player %03d: Found court %03d, attempting to join\n", player->id, court_id);
 	char court_fifo_name[MAX_FIFO_NAME_LEN];
 	get_court_fifo_name(court_id, court_fifo_name);
@@ -336,7 +336,7 @@ void player_join_court(player_t* player, unsigned int court_id) {
 		player_at_court(player, court_fifo, my_fifo);
 	} else {
 		log_write(INFO_L, "Player %03d: Player was rejected from Court %03d\n", player->id, court_id);
-		// @log kicked_from_court player->id court_id
+		// @checkpoint kicked_from_court player->id court_id
 		player->times_kicked++;
 	}
 	player_unset_sigset_handler();
@@ -355,7 +355,7 @@ void player_join_court(player_t* player, unsigned int court_id) {
  * Returns true if could found a court. */
 bool player_looking_for_court(player_t* player) {
 	log_write(INFO_L, "Player %03d: Looking for a court\n", player->id);
-	// @log looking_court player->id
+	// @checkpoint looking_court player->id
 
 	// Search for a free court
 	int court_id = -1;
@@ -447,7 +447,7 @@ void player_main(unsigned int id, tournament_t* tm) {
 		unsigned long int prob = rand() % 100;
 		if (prob < LEAVING_PROB) {
 			log_write(INFO_L, "Player %03d: Decided to leave the tournament on his own!\n", player->id);
-			// @log leave_tournament player->id
+			// @checkpoint leave_tournament player->id
 			sem_post(sem_start, 1);
 			lock_acquire(tm->tm_lock);
 			tm->tm_data->tm_on_beach_players--;
@@ -457,7 +457,7 @@ void player_main(unsigned int id, tournament_t* tm) {
 
 		if (prob < RESTING_PROB) {
 			log_write(INFO_L, "Player %03d: Decided to leave the beach!\n", player->id);
-			// @log leave_beach player->id
+			// @checkpoint leave_beach player->id
 			sem_post(sem_start, 1);
 			lock_acquire(tm->tm_lock);
 			tm->tm_data->tm_on_beach_players--;
@@ -470,7 +470,7 @@ void player_main(unsigned int id, tournament_t* tm) {
 			tm->tm_data->tm_on_beach_players++;
 			lock_release(tm->tm_lock);
 			log_write(INFO_L, "Player %03d: Has re-entered the beach successfully\n", player->id);
-			// @log enter_beach player->id
+			// @checkpoint enter_beach player->id
 			continue;
 		}
 
@@ -498,8 +498,8 @@ void player_main(unsigned int id, tournament_t* tm) {
 	lock_release(player->tm->tm_lock);
 
 	log_write(INFO_L, "Player %03d: Now leaving\n", player->id);
-	// @log leave_beach player->id
-	// @log leave_tournament player->id
+	// @checkpoint leave_beach player->id
+	// @checkpoint leave_tournament player->id
 	player_destroy(player);
 	log_close();
 	exit(0);
